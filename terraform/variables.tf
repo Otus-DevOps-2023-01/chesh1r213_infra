172682e720f1d00eb845b112879f748faba08931
System Administrator <root@MacBook-Air-Otter.local>
2023-03-15 18:15:30 +0300

    terrform1

---
+++ terraform/variables.tf
@@ -0,0 +1,27 @@
+variable cloud_id {
+  description = "Cloud"
+}
+variable folder_id {
+  description = "Folder"
+}
+variable zone {
+  description = "Zone"
+  # Значение по умолчанию
+  default = "ru-central1-a"
+}
+variable token {
+  # Описание переменной
+  description = "token ya"
+}
+variable public_key_path {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+variable subnet_id {
+  # Описание переменной
+  description = "sub"
+}
+variable image_id {
+  # Описание переменной
+  description = "image packer"
+}
